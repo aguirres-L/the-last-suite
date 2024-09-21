dcb2fa1c87495f35b8b8dced5fafa84091bb63f6
Agustin Aguirres <109905284+aguirres-L@users.noreply.github.com>
2024-09-21 11:38:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'the-last-suite',
+  webDir: 'www'
+};
+
+export default config;
